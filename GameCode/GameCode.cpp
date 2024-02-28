395655f3359ebaefcab6e145abafe59326166993
Igor Motyka <igor_motyka@mail.com>
2024-02-28 15:38:33 +0100

    Moved declarations from header to cpp

--- GameCode/GameCode.cpp
+++ GameCode/GameCode.cpp
@@ -1,6 +1,26 @@
-#include "pch.h"
 #include "GameCode.h"
 
 void GameUpdateAndRender()
 {
 }
+
+const Gradient card_gradient {
+		GradientType::Radial,
+		v2(0.5, 0.5),
+		{0.777, -0.56},
+		1.0,
+		{YELLOW, BLACK},
+};
+
+const Gradient header_gradient {
+    GradientType::ThreeColor,
+    v2(0.25, 0.65),
+    { 0.0, 0.0 },
+    1.0,
+    { RED, BLACK, YELLOW },
+};
+
+EXPORT const Constants cons {
+	header_gradient,
+	card_gradient,
+};
--- GameCode/GameCode.h
+++ GameCode/GameCode.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Constants.h";
+#include "Constants.h"
 #include <array>
 
 #define EXPORT extern "C" __declspec(dllexport)
@@ -25,27 +25,6 @@ typedef void (*tGameUpdateAndRender) (void);
 struct Constants {
 	Gradient header_gradient;
 	Gradient card_gradient;
-};
-
-const Gradient card_gradient {
-		GradientType::Radial,
-		v2(0.5, 0.5),
-		{0.777, -0.56},
-		1.0,
-		{YELLOW, BLACK},
-};
-
-const Gradient header_gradient {
-    GradientType::ThreeColor,
-    v2(0.25, 0.65),
-    { 0.0, 0.0 },
-    1.0,
-    { RED, BLACK, YELLOW },
-};
-
-EXPORT const Constants cons {
-	header_gradient,
-	card_gradient,
 };
 
 EXPORT void GameUpdateAndRender();
