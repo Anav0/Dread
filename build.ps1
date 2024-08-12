a5edc7c2ea5576903d93ab00501cfc350f7c4519
Igor Motyka <igor_motyka@mail.com>
2024-08-12 16:26:46 +0200

    Fixed compile script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,1 @@
-msbuild "./Engine/Engine.vcxproj" /p:Configuration="Debug" /p:Verbosity="Minimal" --clp:ErrorsOnly /p:Platform="x64" /p:OutDir="..\bin\Debug"
-msbuild "./Game/Game.vcxproj" /p:Configuration="Debug" /p:Verbosity="Minimal" --clp:ErrorsOnly /p:Platform="x64" /p:OutDir="..\bin\Debug"
-msbuild "./Window/Window.vcxproj" /p:Configuration="Debug" /p:Verbosity="Minimal" --clp:ErrorsOnly /p:Platform="x64" /p:OutDir="..\bin\Debug"
+msbuild "Dread.sln" /p:Configuration="Analysis" /p:Verbosity="Minimal" --clp:ErrorsOnly /p:Platform="x64"
